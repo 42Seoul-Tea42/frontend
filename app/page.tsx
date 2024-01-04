866ccd0c31e3acbc6479456c66a86fe9306ad470
JaeJunday <wowns8114@gmail.com>
2024-01-04 12:30:10 +0900

    feat: 랜딩페이지 버튼 추가
    - learn more 버튼 추가
    - 부드러운 스크롤 함수 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,55 @@
 
 import { useRouter } from "next/navigation";
 
-function Page() {
+function LandingPage() {
   const router = useRouter();
+
+  const scrollView = () => {
+    (function smoothScroll() {
+      const targetY = window.innerHeight;
+      const duration = 500;
+
+      const startTime = performance.now();
+      function animateScroll(currentTime: number) {
+        const elapsedTime = currentTime - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+
+        window.scrollTo(0, targetY * progress);
+
+        if (elapsedTime < duration) {
+          window.requestAnimationFrame(animateScroll);
+        }
+      }
+      window.requestAnimationFrame(animateScroll);
+    })();
+  };
+
   return (
     <>
-      <h1>Tea for Two</h1>
-      <p className="bg-blue-500 mt-2">This is a test page for Tailwind CSS.</p>
-      <button className="bg-green" onClick={() => router.push("/home")}>
-        Get Started
-      </button>
+      {/* Tea for Two Main */}
+      <div className="w-full mx-auto h-screen bg-blue-100">
+        <div className="flex items-center justify-center h-screen">
+          <h1 className="text-center bg-red-100">Tea for Two</h1>
+          <button
+            type="button"
+            className="text-black bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            onClick={scrollView}
+          >
+            Learn More
+          </button>
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            onClick={() => router.push("/home")}
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
+      {/* Learn more */}
+      <div className="w-full mx-auto p-4 h-screen bg-red-400"></div>
     </>
   );
 }
 
-export default Page;
+export default LandingPage;
