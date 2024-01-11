01d59081f042759ce435abb400b53a9f48265317
JaeJunday <wowns8114@gmail.com>
2024-01-11 20:07:10 +0900

    feat: 홈 카드 컴포넌트 추가
    메인화면 캐러셀 대신 카드로 구현

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,97 @@
+"use client";
+
+import { useEffect } from "react";
+import Image from "next/image";
+
 function Home() {
+  const clickImage = () => {
+    // 상세데이터 가져오기
+  };
+
+  const changeNextImgage = () => {
+    // 다음사람 이미지 가져오기
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <>
-      <h1 className="w-full bg-green-100 h-screen">캐러셀</h1>
+      <div className="mx-auto max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div
+          id="default-carousel"
+          className="relative w-full"
+          data-carousel="slide"
+        >
+          <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+            <div className="duration-700 ease-in-out" data-carousel-item>
+              <Image
+                src="/조유리.webp"
+                alt="face"
+                width={500}
+                height={700}
+                priority={true}
+              />
+            </div>
+            <div className="duration-700 ease-in-out" data-carousel-item>
+              <Image
+                src="/장원영.jpeg"
+                alt="face"
+                width={500}
+                height={700}
+                priority={true}
+              />
+            </div>
+          </div>
+          <button
+            type="button"
+            className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-prev
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+              <span className="sr-only">Previous</span>
+            </span>
+          </button>
+          <button
+            type="button"
+            className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-next
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="sr-only">Next</span>
+            </span>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
