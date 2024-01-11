cd67e34024fec848755369c8b90a9d01e8fdc417
JaeJunday <wowns8114@gmail.com>
2024-01-11 20:05:57 +0900

    fix: meta tag 수정
    next  layout 에서 Metadata 객체를 export 하는 방식으로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import "./styles/tailwind.css";
 import Head from "next/head";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tea For Two",
+  description: "Tea For Two is a social media platform for tea lovers.",
+};
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <html>
+      <html lang="en">
         <Head>
           <title>tea for two</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ function LandingPage() {
       <div className="w-full mx-auto h-screen bg-blue-100">
         <div className="flex items-center justify-center h-screen">
           <div>
-            <h1 className="text-center bg-red-100">Tea for Two</h1>
+            <h1 className="text-center bg-red-100">Tea For Two</h1>
             <button
               type="button"
               className="text-black bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
