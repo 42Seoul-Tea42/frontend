f982d18660ef91c5f47514b287095f94c494d374
JaeJunday <wowns8114@gmail.com>
2024-01-30 17:06:40 +0900

    feat auth 버튼 각각의 컴포넌트로 분리

---
+++ app/auth/login/EmailLoginButton.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useDispatch } from 'react-redux';
+import { toggleEmailView } from '../../store/slices/loginViewSlice';
+
+const EmailLoginButton: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const useToggleEmailView = () => dispatch(toggleEmailView());
+
+  return (
+    <button
+      onClick={useToggleEmailView}
+      className="text-white bg-[#ff0000] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
+    >
+      이메일 로그인
+    </button>
+  );
+};
+
+export default EmailLoginButton;
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,40 +1,31 @@
 'use client';
 
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 import EmailLoginForm from './EmailLoginForm';
 import GoogleLoginButton from './GoogleLoginButton';
 import KakaoLoginButton from './KakaoLoginButton';
+import EmailLoginButton from './EmailLoginButton';
 
 const LoginPage: React.FC = () => {
-	const [isEmail, setIsEmail] = useState<boolean>(true);
+  const emailFormView = useSelector((state: RootState) => state.loginViewer.email);
 
-	const toggleEmailLoginForm = () => {
-		setIsEmail(!isEmail);
-	};
-
-	return (
-		<div className="flex items-center justify-center w-full h-screen bg-white">
-			{isEmail ? (
-				<>
-					<div className="max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-						<KakaoLoginButton />
-						<GoogleLoginButton />
-						<button onClick={toggleEmailLoginForm}>이메일 로그인</button>
-					</div>
-				</>
-			) : (
-				<>
-					<button onClick={toggleEmailLoginForm} className="mr-4">
-						<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-							<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13" />
-						</svg>
-					</button>
-					<EmailLoginForm />
-				</>
-			)}
-			{/* 이메일로그인 */}
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-white">
+      <div className="max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="text-xl font-medium text-gray-900 dark:text-white">Welcome to tea for two !</h5>
+        {!emailFormView ? (
+          <>
+            <KakaoLoginButton />
+            <GoogleLoginButton />
+            <EmailLoginButton />
+          </>
+        ) : (
+          <EmailLoginForm />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
