4e80abe0a57850ca7441ba7a2431d3b2bc5a8ed5
JaeJunday <wowns8114@gmail.com>
2024-01-30 17:04:46 +0900

    feat email form 토글 버튼 슬라이스에 추가
    - layout에서 auth 내부 자식 페이지 컴포넌트 store로 감싸기
    - 버튼 뷰와 로그인폼, 로그인 버튼 분리

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,12 +1,14 @@
 // src/store/index.ts
 
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './slice';
+import loginViewSlice from './slices/loginViewSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    loginViewer: loginViewSlice
+  }
 });
+
+export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
---
+++ app/store/slices/loginViewSlice.ts
@@ -0,0 +1,25 @@
+// src/store/slice.ts
+
+import { createSlice } from '@reduxjs/toolkit';
+
+interface LoginViewState {
+  email: boolean;
+}
+
+const initialState: LoginViewState = {
+  email: false
+};
+
+const loginViewSlice = createSlice({
+  name: 'loginView',
+  initialState,
+  reducers: {
+    toggleEmailView: state => {
+      state.email = !state.email;
+    }
+  }
+});
+
+export const { toggleEmailView } = loginViewSlice.actions;
+
+export default loginViewSlice.reducer;
