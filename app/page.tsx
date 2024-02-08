3b3d0c5ab41360d77b34cc3bc68524549419df0d
jaejun kim <wowns8114@gmail.com>
2024-02-08 08:06:23 +0000

    feat 메인페이지 점보버튼으로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -24,23 +24,20 @@ function LandingPage() : React.ReactNode {
       <Provider store={store}>
         {/* Main page - Tea for Two */}
         <div className="w-full mx-auto h-screen bg-blue-100">
+            <h1 className="text-center bg-red-100">Tea For Two</h1>
           <div className="flex items-center justify-center h-screen">
             <div>
-              <h1 className="text-center bg-red-100">Tea For Two</h1>
-              <button
-                type="button"
-                className="text-black bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                onClick={scrollPage}
-              >
-                Learn More
-              </button>
-              <button
-                type="button"
-                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                onClick={pushLoginPage}
-              >
-                Get Started
-              </button>
+              <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                  <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                      Get started
+                      <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
+                  <a href="#" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                      Learn more
+                  </a>  
+              </div>
               <Cup />
             </div>
           </div>
