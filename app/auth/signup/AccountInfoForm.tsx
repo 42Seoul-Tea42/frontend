98df9919e5959d72b9fca727665929e2b82f8bc2
JaeJunday <wowns8114@gmail.com>
2024-02-26 20:19:47 +0900

    style signup form의 가로세로 최소값 지정
    min-w-96, min-h-96

--- app/auth/signup/AccountInfoForm.tsx
+++ app/auth/signup/AccountInfoForm.tsx
@@ -11,9 +11,9 @@ const AccountInfoForm: React.FC = () => {
   };
 
   return (
-    <form className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <form className="max-w-sm min-w-96 min-h-96 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <h5 className="text-lg font-semibold mb-5 underline decoration-wavy decoration-pink-500/50">
-        회원가입을 위해서 필수 정보를 입력해주세요.
+        회원가입을 위한 계정정보를 입력해주세요.
       </h5>
       <div className="relative z-0 w-full mb-5 group">
         <input
@@ -113,23 +113,21 @@ const AccountInfoForm: React.FC = () => {
           </label>
         </div>
       </div>
-      <div className="grid md:grid-cols-2 md:gap-6">
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="number"
-            name="floating_company"
-            id="floating_company"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="floating_company"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Age
-          </label>
-        </div>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="number"
+          name="floating_company"
+          id="floating_company"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="floating_company"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Age
+        </label>
       </div>
       <button
         type="submit"
