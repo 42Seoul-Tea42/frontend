75e82e309a6d8bf98684f5db81de1b6d9b0c70d5
JaeJunday <wowns8114@gmail.com>
2024-02-26 20:19:03 +0900

    feat personal 폼 안의 radio 버튼을 컴포넌트로 분리

---
+++ app/auth/signup/GenderRadioList.tsx
@@ -0,0 +1,46 @@
+const GenderRadioList = () => {
+  return (
+    <div>
+      <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+          <div className="flex items-center ps-3">
+            <input
+              id="male"
+              type="radio"
+              value="male"
+              name="gender"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              required
+            />
+            <label
+              htmlFor="male"
+              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              남성
+            </label>
+          </div>
+        </li>
+        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+          <div className="flex items-center ps-3">
+            <input
+              id="woman"
+              type="radio"
+              value="woman"
+              name="gender"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              required
+            />
+            <label
+              htmlFor="woman"
+              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              여성
+            </label>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default GenderRadioList;
