85d9fded17d90924ce08957e0ccb473e7c7c94d9
JaeJunday <wowns8114@gmail.com>
2024-02-26 20:20:23 +0900

    feat form 바꿔주는 state 추가
    - 현재 step에 따라서 렌더링해주는 form을 바꿔주는 state 추가
    - api 호출 후 성공하면 다음 step으로 넘어가게끔 구현 필요

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,18 +1,40 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import AccountInfoForm from './AccountInfoForm';
 import SignupStepper from './SignupStepper';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import PersonalInfoForm from './PersonalInfoForm';
+import { signupSteps } from '../../store/slices/signupSlice';
 
 const Signup: React.FC = () => {
   const currentStep = useSelector((state: RootState) => state.signupViewer.currentStep);
+  const [currentForm, setCurrentForm] = useState<JSX.Element | null>(null);
+
+  useEffect(() => {
+    switch (currentStep) {
+      case signupSteps.AccountInfo:
+        setCurrentForm(<AccountInfoForm />);
+        break;
+      case signupSteps.PersonalInfo:
+        setCurrentForm(<PersonalInfoForm />);
+        break;
+      case signupSteps.EmojiInfo:
+        setCurrentForm(<div>Emoji Info</div>);
+        break;
+      default:
+        setCurrentForm(<AccountInfoForm />);
+        break;
+    }
+  }, [currentStep]);
+
   return (
     <div className="items-center justify-center h-screen flex flex-col">
       <div className="mb-20">
         <SignupStepper currentStep={currentStep} />
       </div>
-      <AccountInfoForm />
+      {currentForm}
     </div>
   );
 };
