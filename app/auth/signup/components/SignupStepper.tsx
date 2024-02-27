38bd6d42392add9f1ccf12a7c4ff5885a03b840f
JaeJunday <wowns8114@gmail.com>
2024-02-27 20:45:37 +0900

    feat 회원가입 스탭퍼 색깔 구현
    - 완료된 폼의 스태퍼 색깔을 파란색으로 변경하도록 수정
    - 인덱스도 svg로 변경

--- app/auth/signup/components/SignupStepper.tsx
+++ app/auth/signup/components/SignupStepper.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import { Fragment } from 'react';
 import { signupSteps } from '../../../store/slices/signupSlice';
 import StepperSVG from '../../../svg/StepperSVG';
+
+interface StepperItem {
+  step: number;
+  label: string;
+}
+
+const steps: StepperItem[] = [
+  { step: signupSteps.AccountInfo, label: 'Account Info' },
+  { step: signupSteps.PersonalInfo, label: 'Personal Info' },
+  { step: signupSteps.ProfileUpload, label: 'Profile Upload' },
+  { step: signupSteps.EmojiInfo, label: 'Emoji Info' }
+];
 
 interface SignupStepperProps {
   currentStep: number;
@@ -10,36 +23,24 @@ interface SignupStepperProps {
 const SignupStepper: React.FC<SignupStepperProps> = ({ currentStep }) => {
   return (
     <ol className="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
-      <li className="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-        <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-          {currentStep > signupSteps.AccountInfo ? <StepperSVG /> : <span className="me-2">1</span>}
-          Account <span className="hidden sm:inline-flex sm:ms-2">Info</span>
-        </span>
-      </li>
-      <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-        <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-          {currentStep > signupSteps.PersonalInfo ? (
-            <StepperSVG />
-          ) : (
-            <span className="me-2">2</span>
-          )}
-          Personal <span className="hidden sm:inline-flex sm:ms-2">Info</span>
-        </span>
-      </li>
-      <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-        <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-          {currentStep > signupSteps.ProfileUpload ? (
-            <StepperSVG />
-          ) : (
-            <span className="me-2">3</span>
-          )}
-          Profile <span className="hidden sm:inline-flex sm:ms-2">Upload</span>
-        </span>
-      </li>
-      <li className="flex items-center">
-        {currentStep > signupSteps.EmojiInfo ? <StepperSVG /> : <span className="me-2">4</span>}
-        Emoji <span className="hidden sm:inline-flex sm:ms-2">Info</span>
-      </li>
+      {steps.map((item, index) => (
+        <li
+          key={index}
+          className={`flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700 ${
+            currentStep > item.step ? 'text-blue-600 dark:text-blue-500' : ''
+          }`}
+        >
+          <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+            {currentStep > item.step ? <StepperSVG /> : <span className="me-2">{index + 1}</span>}
+            {item.label.split(' ').map((word, idx) => (
+              <Fragment key={idx}>
+                {idx > 0 && <span className="hidden sm:inline-flex sm:ms-2"> </span>}
+                <span>{word}</span>
+              </Fragment>
+            ))}
+          </span>
+        </li>
+      ))}
     </ol>
   );
 };
