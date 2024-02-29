b5cb17b4f0aedf75bdb45e51972befd6da1d8ca4
JaeJunday <wowns8114@gmail.com>
2024-02-29 17:45:04 +0900

    feat 검색 페이지 아코디언으로 구현

--- app/home/(routes)/search/page.tsx
+++ app/home/(routes)/search/page.tsx
@@ -1,16 +1,27 @@
 'use client';
 
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import InputRangeStarBar from './components/InputRangeStarBar';
-import InputRangeAgeBar from './components/InputRangeAgeBar';
 import { RootState } from '../../../store';
 import fetchApi from '../../../utils/api';
+import TagSelector from '../../../auth/signup/components/TagSelector';
+import InputRangeStarBar from './components/InputRangeStarBar';
+import DirectionSVG from '../../../svg/DirectionSVG';
+import InputMinMaxAge from './components/InputMinMaxAge';
 
-const Search = () => {
+const Search: React.FC = () => {
   const ageRange = useSelector((state: RootState) => state.searchValue.age);
   const distance = useSelector((state: RootState) => state.searchValue.distance);
   const fameRate = useSelector((state: RootState) => state.searchValue.fame);
   const tags = useSelector((state: RootState) => state.searchValue.tags);
+
+  const [isOpen, setIsOpen] = useState<boolean[]>([false, false, false, false]);
+
+  const toggleAccordion = (index: number) => {
+    setIsOpen((prevState: boolean[]) =>
+      prevState.map((state, idx) => (idx === index ? !state : false))
+    );
+  };
 
   const handleSearchButton = () => {
     const sendData = {
@@ -24,20 +35,56 @@ const Search = () => {
     fetchApi('/user/search', 'POST', JSON.stringify(sendData));
   };
 
+  const accordionItems = [
+    { title: '검색할 나이를 선택해주세요.', content: <InputMinMaxAge /> },
+    {
+      title: '검색할 최대 거리를 선택해주세요.',
+      content: <input type="range" min="20" max="100" step={1} />
+    },
+    { title: '검색할 태그를 선택해주세요.', content: <TagSelector /> },
+    { title: '검색할 등급을 선택해주세요.', content: <InputRangeStarBar /> }
+  ];
+
   return (
-    <>
-      <h1 className="w-full bg-red-100 h-screen">
-        <InputRangeAgeBar />
-        <InputRangeStarBar />
-        <button
-          type="button"
-          className="text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-          onClick={handleSearchButton}
-        >
-          검색
-        </button>
-      </h1>
-    </>
+    <div className="flex justify-center h-screen">
+      <div className="w-1/3 min-w-96">
+        {accordionItems.map((item, index) => (
+          <div key={index} id={`accordion-collapse-${index}`} data-accordion="collapse">
+            <h2 id={`accordion-collapse-heading-${index}`} className="max-h-12">
+              <button
+                type="button"
+                className="flex max-h-12 items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                data-accordion-target={`#accordion-collapse-body-${index}`}
+                aria-expanded={isOpen[index] ? 'true' : 'false'}
+                aria-controls={`accordion-collapse-body-${index}`}
+                onClick={() => toggleAccordion(index)}
+              >
+                {item.title}
+                <DirectionSVG direction={isOpen[index] ? 'top' : 'down'} />
+              </button>
+            </h2>
+            <div
+              id={`accordion-collapse-body-${index}`}
+              className={`${isOpen[index] ? '' : 'hidden'}`}
+              aria-labelledby={`accordion-collapse-heading-${index}`}
+            >
+              <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                {item.content}
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="flex justify-end mt-10">
+          <button
+            type="button"
+            className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={() => {}}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
