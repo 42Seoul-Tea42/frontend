69f8b182892ea40807149cc85bfd76aabd415294
JaeJunday <wowns8114@gmail.com>
2024-03-01 16:26:32 +0900

    feat input number 컴포넌트 재사용을 위한 분리
    고차함수를 사용해서 inputnumber 컴포넌트의 재사용성을 높임

--- app/home/(routes)/search/components/InputMinMaxAge.tsx
+++ app/home/(routes)/search/components/InputMinMaxAge.tsx
@@ -1,5 +1,28 @@
-const InputMinMaxAge: React.FC = () => {
-  return <div>{<input type="range" min="20" max="100" step={1} />}</div>;
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import InputNumber from '../../../../components/InputNumber';
+import { setMaxAge, setMinAge } from '../../../../store/slices/searchValueDataSlice';
+import { RootState } from '../../../../store';
+
+export const InputMinMaxAge: React.FC = () => {
+  const dispatch = useDispatch();
+  const minAgeState = useSelector((state: RootState) => state.searchValue.minAge);
+  const maxAgeState = useSelector((state: RootState) => state.searchValue.maxAge);
+
+  const handleMinAgeChange = (minAge: number) => {
+    dispatch(setMinAge(minAge));
+  };
+
+  const handleMaxAgeChange = (maxAge: number) => {
+    dispatch(setMaxAge(maxAge));
+  };
+
+  return (
+    <div className="flex grid grid-cols-2">
+      <InputNumber text="min" handle={handleMinAgeChange} viewControl={minAgeState} />
+      <InputNumber text="max" handle={handleMaxAgeChange} viewControl={maxAgeState} />
+    </div>
+  );
 };
 
 export default InputMinMaxAge;
