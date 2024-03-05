020cca4570dc9e0d44f62c2a47322d901a23365d
JaeJunday <wowns8114@gmail.com>
2024-03-05 16:36:01 +0900

    style 로그인 페이지 버튼 간격조정
    - 버튼 위치를 로그인 폼 밖으로 변경

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -16,32 +16,28 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      <div className="max-w-md w-2/5 h-2/6 min-w-96 min-h-96 flex flex-col text-center items-center justify-center bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <div className="flex items-end gap-3">
-          {emailFormView && (
-            <div className="left-0">
-              <ToggleEmailFormButton handle={toggle} />
-            </div>
-          )}
-          <h5 className="text-3xl min-w-96 font-semibold text-gray-900 dark:text-white flex-grow">
+      <div className="max-w-100 min-w-96 min-h-96 flex flex-col text-center items-center justify-center bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex flex-row ml-5 mr-5 mt-5">
+          <ToggleEmailFormButton handle={toggle} />
+          <h5 className="text-3xl ml-5 min-w-80 font-semibold text-gray-900 dark:text-white flex-grow">
             Welcome to tea for two!
           </h5>
         </div>
-        <div className="flex flex-col items-center justify-center min-h-80 gap-2">
-          {!emailFormView ? (
-            <>
-              <GoogleLoginButton />
-              <KakaoLoginButton />
-              <h6 className="text-md mb-2 text-gray-600"> or </h6>
-              <EmailLoginButton handle={toggle} />
-            </>
-          ) : (
-            <>
-              <EmailLoginForm />
-            </>
-          )}
+        {!emailFormView ? (
+          <div className="flex flex-col items-center justify-center min-h-80 gap-2">
+            <GoogleLoginButton />
+            <KakaoLoginButton />
+            <h6 className="text-md mb-2 text-gray-600"> or </h6>
+            <EmailLoginButton handle={toggle} />
+          </div>
+        ) : (
+          <div className="mt-10 mb-10">
+            <EmailLoginForm />
+          </div>
+        )}
+        <div className="mb-5">
+          <CreateAccountButton />
         </div>
-        <CreateAccountButton />
       </div>
     </div>
   );
