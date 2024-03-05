191129ecda005908bb3563f0a22da3644d905b24
JaeJunday <wowns8114@gmail.com>
2024-03-05 17:52:12 +0900

    style chat message form border 설정
    border, border-radious 설정

--- app/home/(routes)/chat/components/SendMessageForm.tsx
+++ app/home/(routes)/chat/components/SendMessageForm.tsx
@@ -4,16 +4,16 @@ const SendMessageForm = () => {
       <label htmlFor="chat" className="sr-only">
         Your message
       </label>
-      <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
+      <div className="flex items-center px-3 py-3 rounded-b-xl border bg-gray-50 dark:bg-gray-700">
         <textarea
           id="chat"
           rows={1}
-          className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-400 focus:border-blue-400 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-400 dark:focus:border-blue-400"
           placeholder="Your message... (.../500)"
         ></textarea>
         <button
           type="submit"
-          className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+          className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-400 dark:hover:bg-gray-600"
         >
           <svg
             className="w-5 h-5 rotate-90 rtl:-rotate-90"
--- app/home/(routes)/chat/components/ViewMessageForm.tsx
+++ app/home/(routes)/chat/components/ViewMessageForm.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const ViewMessageForm = () => {
   return (
-    <div className="flex items-start gap-2.5">
+    <div className="flex border p-5 rounded-t-xl h-2/5 min-h-96 items-start gap-2.5">
       <Image
         className="w-8 h-8 rounded-full"
         src="/장원영.jpeg"
@@ -14,16 +14,11 @@ const ViewMessageForm = () => {
       />
       <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
         <div className="flex items-center space-x-2 rtl:space-x-reverse">
-          <span className="text-sm font-semibold text-gray-900 dark:text-white">
-            test user 1
-          </span>
-          <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-            11:46
-          </span>
+          <span className="text-sm font-semibold text-gray-900 dark:text-white">test user 1</span>
+          <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
         </div>
         <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
-          That's awesome. I think our users will really appreciate the
-          improvements.
+          That's awesome. I think our users will really appreciate the improvements.
         </p>
       </div>
     </div>
