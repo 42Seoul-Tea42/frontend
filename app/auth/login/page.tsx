08f78ec17d3857f4ca981e3f0d0080c1431d150f
JaeJunday <wowns8114@gmail.com>
2024-03-06 17:41:15 +0900

    style email toggle view 상태에 따라 hidden 처리
    - 방향을 바꾸는 동작에서 hidden 처리로 변경

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -17,8 +17,10 @@ const LoginPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <div className="max-w-100 min-w-96 min-h-96 flex flex-col text-center items-center justify-center bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <div className="flex flex-row ml-5 mr-5 mt-5">
-          <ToggleEmailFormButton handle={toggle} state={emailFormView} />
+        <div className="flex flex-row items-end ml-5 mr-5 mt-5">
+          <div className={`${emailFormView ? '' : 'hidden'}`}>
+            <ToggleEmailFormButton handle={toggle} state={emailFormView} />
+          </div>
           <h5 className="tracking-wide text-3xl ml-5 min-w-80 font-semibold text-gray-900 dark:text-white flex-grow">
             Welcome to tea for two!
           </h5>
