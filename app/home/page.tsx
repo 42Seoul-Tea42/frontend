efc5326858599333e35c559fbe3910b2792a3d1c
JaeJunday <wowns8114@gmail.com>
2024-03-07 14:49:57 +0900

    fix 이벤트 중복 등록으로 인한 문제 수정
    - next component인 Image에서 기본값이 draggable=true 로 설정되어있어 이미지가 프리렌더링 되면서 드래그가 두번 발생하는 문제를 제거

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,34 +1,27 @@
 'use client';
 
-import { useEffect } from 'react';
 import Image from 'next/image';
-import CarouselButton from './components/CarouselButton';
+import Draggable from 'react-draggable';
 
 const Home: React.FC = () => {
-  // const clickImage = () => {
-  // 상세데이터 가져오기
-  // };
-
-  // const changeNextImgage = () => {
-  // 다음사람 이미지 가져오기
-  // };
-
-  useEffect(() => {}, []);
-
   return (
-    <>
-      <div className="w-full h-screen">
+    <div className="w-full h-screen">
+      {' '}
+      <Draggable>
         <div className="mx-auto mt-20 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <div id="default-carousel" className="relative w-full" data-carousel="slide">
-            <div className="relative h-96 overflow-hidden rounded-lg md:h-96">
-              <Image src="/조유리.webp" alt="face" width={500} height={500} priority={true} />
-            </div>
-            {/* <CarouselButton direction="left" position="start-0" />
-              <CarouselButton direction="right" position="end-0" /> */}
+          <div className="relative overflow-hidden rounded-lg md:h-96">
+            <Image
+              src="/조유리.webp"
+              alt="face"
+              width={500}
+              height={500}
+              priority={true}
+              draggable={false}
+            />
           </div>
         </div>
-      </div>
-    </>
+      </Draggable>
+    </div>
   );
 };
 
