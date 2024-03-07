38655e12323a8a2db0fdf58530b4a26ec4b19aea
JaeJunday <wowns8114@gmail.com>
2024-03-07 16:19:59 +0900

    feat 드래그 후 튕기는 효과 추가, 일정 축 이상 드래그시 액션 핸들러 추가
    드래그시 동작과 일정 드래그 후 효과를 추가

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,58 @@
 'use client';
 
 import Image from 'next/image';
-import Draggable from 'react-draggable';
+import { useEffect, useState } from 'react';
+import Draggable, { DraggableEventHandler } from 'react-draggable';
 
 const Home: React.FC = () => {
+  const [springEffect, setspringEffect] = useState<boolean>(false);
+  const [isAction, setIsAction] = useState<boolean>(false);
+  const [originalPosition, setOriginalPosition] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0
+  });
+
+  const handleDragStart: DraggableEventHandler = (_e, data) => {
+    setspringEffect(true);
+  };
+
+  const handleDrag: DraggableEventHandler = (_e, data) => {
+    if (Math.abs(data.x) > 250) {
+      setIsAction(true);
+    } else {
+      setIsAction(false);
+    }
+  };
+
+  const handleDragStop: DraggableEventHandler = (_e, data) => {
+    setspringEffect(false);
+  };
+
+  useEffect(() => {
+    setOriginalPosition({ x: 0, y: 0 });
+  }, [springEffect]);
+
+  useEffect(() => {
+    if (isAction) {
+      //data patch
+    }
+  }, [isAction]);
+
   return (
     <div className="w-full h-screen">
-      {' '}
-      <Draggable>
-        <div className="mx-auto mt-20 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <div className="relative overflow-hidden rounded-lg md:h-96">
+      <Draggable
+        axis="x"
+        onStart={handleDragStart}
+        onDrag={handleDrag}
+        onStop={handleDragStop}
+        position={{ x: originalPosition.x, y: originalPosition.y }}
+      >
+        <div className="mx-auto mt-20 max-w-sm bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+          <div
+            className={`relative overflow-hidden rounded-lg h-96 ${
+              isAction ? 'brightness-50 shadow-2xl' : ''
+            }`}
+          >
             <Image
               src="/조유리.webp"
               alt="face"
