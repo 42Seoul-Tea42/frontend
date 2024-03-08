9c467980644706ba42abe0adef7640478513de0f
JaeJunday <wowns8114@gmail.com>
2024-03-08 18:16:40 +0900

    refactor feat drag, action상태 추적을 위한 state ImageConverter로 이동

--- app/(pages)/home/components/ImageConverter.tsx
+++ app/(pages)/home/components/ImageConverter.tsx
@@ -1,29 +1,30 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import Draggable, { DraggableEventHandler } from 'react-draggable';
+import { useDispatch } from 'react-redux';
 
-interface ImageConverterProps {
-  isAction: boolean;
-  isDragging: boolean;
-  setIsAction: (isAction: boolean) => void;
-}
-
-const ImageConverter: React.FC<ImageConverterProps> = ({ isAction, setIsAction, isDragging }) => {
+const ImageConverter: React.FC = () => {
+  const dispatch = useDispatch();
+  const [isAction, setIsAction] = useState<boolean>(false);
+  const [direction, setDirection] = useState<'left' | 'right' | 'none'>('none');
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [originalPosition, setOriginalPosition] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0
+  });
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [images, setImages] = useState<string[]>([]);
 
-  const renderImages = () => {
-    setImages([...images, '/emoji/1.jpg']);
+  const handleDrag: DraggableEventHandler = (_e, data) => {
+    setIsDragging(true);
+    setIsAction(Math.abs(data.x) > 250);
+    setDirection(data.x > 0 ? 'right' : 'left');
   };
 
-  useEffect(() => {
-    if (isAction) {
-      // 이미지 프리렌더링
-      if (currentImageIndex <= images.length - 1) {
-        console.log('rendering');
-        renderImages();
-      }
-    }
-  }, [isAction]);
+  const handleDragStop: DraggableEventHandler = () => {
+    setIsDragging(false);
+    setOriginalPosition({ x: 0, y: 0 });
+  };
 
   const handleNextImage = () => {
     if (isAction && !isDragging) {
@@ -32,6 +33,28 @@ const ImageConverter: React.FC<ImageConverterProps> = ({ isAction, setIsAction, 
     }
   };
 
+  const handleFancy = () => {
+    //
+  };
+
+  const renderImages = () => {
+    setImages([...images, '/emoji/1.jpg', '/emoji/2.jpg', '/emoji/3.jpg']);
+  };
+
+  // 이미지 데이터 컨트롤
+  useEffect(() => {
+    if (isAction) {
+      // 이미지 프리렌더링
+      if (currentImageIndex === images.length - 1) {
+        console.log('rendering');
+        renderImages();
+      }
+      if (direction === 'right') handleFancy();
+      else handleNextImage;
+    }
+  }, [isAction]);
+
+  // 드래그 트리거
   useEffect(() => {
     handleNextImage();
     console.log(images.length);
@@ -42,17 +65,35 @@ const ImageConverter: React.FC<ImageConverterProps> = ({ isAction, setIsAction, 
   }, []);
 
   return (
-    <div className="mx-auto max-w-sm bg-white border border-gray-200 rounded-xl dark:bg-gray-800 dark:border-gray-700">
-      <Image
-        src={images[currentImageIndex]}
-        alt="face"
-        width={500}
-        height={500}
-        priority={true}
-        draggable={false}
-        className="rounded-xl"
-      />
-    </div>
+    <>
+      <p
+        className={`text-6xl text-gray-700 font-extrabold text-center absolute top-60 left-0 right-0 ${
+          isDragging ? '' : 'hidden'
+        }`}
+      >
+        {direction === 'left' ? 'next →' : '★ fancy'}
+      </p>
+      <Draggable
+        axis="x"
+        onDrag={handleDrag}
+        onStop={handleDragStop}
+        position={{ x: originalPosition.x, y: originalPosition.y }}
+      >
+        <div className={isAction ? 'brightness-50' : ''}>
+          <div className="mx-auto max-w-sm bg-white border border-gray-200 rounded-xl dark:bg-gray-800 dark:border-gray-700">
+            <Image
+              src={images[currentImageIndex]}
+              alt="face"
+              width={500}
+              height={500}
+              priority={true}
+              draggable={false}
+              className="rounded-xl"
+            />
+          </div>
+        </div>
+      </Draggable>
+    </>
   );
 };
 
--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,49 +1,12 @@
 'use client';
 
-import { useState } from 'react';
-import Draggable, { DraggableEventHandler } from 'react-draggable';
 import ImageConverter from './components/ImageConverter';
 
 const Home: React.FC = () => {
-  const [isAction, setIsAction] = useState<boolean>(false);
-  const [direction, setDirection] = useState<'left' | 'right' | 'none'>('none');
-  const [isDragging, setIsDragging] = useState<boolean>(false);
-  const [originalPosition, setOriginalPosition] = useState<{ x: number; y: number }>({
-    x: 0,
-    y: 0
-  });
-
-  const handleDrag: DraggableEventHandler = (_e, data) => {
-    setIsDragging(true);
-    setIsAction(Math.abs(data.x) > 250);
-    setDirection(data.x > 0 ? 'right' : 'left');
-  };
-
-  const handleDragStop: DraggableEventHandler = () => {
-    setIsDragging(false);
-    setOriginalPosition({ x: 0, y: 0 });
-  };
-
   return (
     <div className="w-full h-screen">
       <div className="mt-40 md:m-40">
-        <p
-          className={`text-6xl text-gray-700 font-extrabold text-center absolute top-60 left-0 right-0 ${
-            isDragging ? '' : 'hidden'
-          }`}
-        >
-          {direction === 'left' ? 'next →' : '★ fancy'}
-        </p>
-        <Draggable
-          axis="x"
-          onDrag={handleDrag}
-          onStop={handleDragStop}
-          position={{ x: originalPosition.x, y: originalPosition.y }}
-        >
-          <div className={isAction ? 'brightness-50' : ''}>
-            <ImageConverter isAction={isAction} setIsAction={setIsAction} isDragging={isDragging} />
-          </div>
-        </Draggable>
+        <ImageConverter />
       </div>
     </div>
   );
