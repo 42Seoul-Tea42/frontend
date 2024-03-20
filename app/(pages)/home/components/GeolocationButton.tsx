2f687afb61297ee8eff6b26895c0812e1387565a
JaeJunday <wowns8114@gmail.com>
2024-03-20 11:31:08 +0900

    feat 위도경도 수동입력폼추가

--- app/(pages)/home/components/GeolocationButton.tsx
+++ app/(pages)/home/components/GeolocationButton.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setLatitude, setLongitude } from '../../../store/slices/signupSlice';
 
 const GeoLocationButton: React.FC = () => {
-  const [latitude, setLatitude] = useState<number | null>(null);
-  const [longitude, setLongitude] = useState<number | null>(null);
-
+  const dispatch = useDispatch();
   const getLocation = () => {
     if (!navigator.geolocation) {
       return;
     }
 
     navigator.geolocation.getCurrentPosition(position => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
+      dispatch(setLatitude(position.coords.latitude));
+      dispatch(setLongitude(position.coords.longitude));
     });
   };
 
+  const handleLatitudeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setLatitude(Number(e.target.value)));
+  };
+
+  const handleLongitudeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setLongitude(Number(e.target.value)));
+  };
+
   return (
-    <div>
+    <div className="flex max-h-12">
       <button
         onClick={getLocation}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
-        Get Location
+        Location
       </button>
-      {latitude !== null && longitude !== null && (
-        <div className="mt-4">
-          <p>Latitude: {latitude}</p>
-          <p>Longitude: {longitude}</p>
-        </div>
-      )}
+      <span>직접입력</span>
+      <input
+        type="number"
+        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        onChange={handleLatitudeChange}
+      />
+      위도
+      <input
+        type="number"
+        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        onChange={handleLongitudeChange}
+      />
+      경도
     </div>
   );
 };
