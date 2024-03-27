88e32905fbe881342f765297a0529ef29d27f8ef
JaeJunday <wowns8114@gmail.com>
2024-03-27 11:09:52 +0900

    feat socket event handler mapping

--- app/utils/socketContext.tsx
+++ app/utils/socketContext.tsx
@@ -4,6 +4,16 @@ import { Events, createSocketOption, registerSocketEvent, unRegisterSocketEvent 
 import { SERVER_URL } from '../../global';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import {
+  newFancy,
+  newHistory,
+  newMatch,
+  readMessage,
+  sendMessage,
+  unMatch,
+  updateDistance,
+  updateStatus
+} from './socketEventHandler';
 
 // SocketContext 생성
 const SocketContext = createContext<Socket | undefined>(undefined);
@@ -38,15 +48,15 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
     if (!socket) return;
 
     const events: Events[] = [
-      // { event: 'new_match', handler: '' },
-      // { event: 'new_fancy', handler: '' },
-      // { event: 'new_history', handler: '' },
-      // { event: 'send_message', handler: '' },
-      // { event: 'read_message', handler: '' },
-      // { event: 'update_distance', handler: '' },
-      // { event: 'update_status', handler: '' },
-      // { event: 'unmatch', handler: '' },
-      // { event: 'unregister', handler: '' }
+      { event: 'new_match', handler: newMatch },
+      { event: 'new_fancy', handler: newFancy },
+      { event: 'new_history', handler: newHistory },
+      { event: 'send_message', handler: sendMessage },
+      { event: 'read_message', handler: readMessage },
+      { event: 'update_distance', handler: updateDistance },
+      { event: 'update_status', handler: updateStatus },
+      { event: 'unmatch', handler: unMatch },
+      { event: 'unregister', handler: unRegister }
     ];
 
     registerSocketEvent(socket, events);
@@ -58,3 +68,6 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
 
   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
 };
+function unRegister(data: any): void {
+  throw new Error('Function not implemented.');
+}
