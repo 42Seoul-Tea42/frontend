3edb2e00f3db9d8a509e29c6032f05c378122273
JaeJunday <wowns8114@gmail.com>
2024-04-07 15:17:37 +0900

    feat 유저를 추상화한 인터페이스 구현

---
+++ app/redux/interface/interface.ts
@@ -0,0 +1,10 @@
+import { Account, AgeGender, Authentication, Identity, Profile, UserRelation } from './submodules/userInterface';
+import { Chatting, RegisterSteps } from './submodules/utilInterface';
+
+// 서비스마다 필요한 user의 정보가 다 달라서 각 서비스에서 사용하는 user를 추상화하고 싶었다. user로 접근했을때 필요한 데이터만 접근할 수 있게하기 위함.
+export interface UserPublicSet extends Identity, AgeGender, UserRelation {}
+export interface UserChattingSet extends Identity, UserRelation, AgeGender, Chatting {}
+export interface UserProfileInquirySet extends Identity, Profile, UserRelation, AgeGender {}
+export interface UserSignupSet extends Identity, Account {}
+export interface UserLoginSet extends Identity, Profile, Authentication, Account, RegisterSteps {}
+export interface UserAccountSet extends Identity, Account, Authentication, AgeGender, Profile, Location {}
