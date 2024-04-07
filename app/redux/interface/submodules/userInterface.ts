55dc56ad08c33db7a1e086a3de0845d091073b93
JaeJunday <wowns8114@gmail.com>
2024-04-07 21:12:18 +0900

    comment interface 타입

--- app/redux/interface/submodules/userInterface.ts
+++ app/redux/interface/submodules/userInterface.ts
@@ -1,44 +1,95 @@
 import { Interests } from './utilInterface';
 
+/**
+ * 사용자의 신원 정보를 나타내는 인터페이스
+ *
+ * - `id`: 사용자의 고유 식별자
+ * - `firstname`: 사용자의 이름
+ * - `lastname`: 사용자의 성
+ */
 export interface Identity {
   id: string;
   firstname: string;
   lastname: string;
 }
 
+/**
+ * 사용자의 나이와 성별 정보를 나타내는 인터페이스
+ *
+ * - `age`: 사용자의 나이
+ * - `gender`: 사용자의 성별
+ */
 export interface AgeGender {
   age: number;
   gender: string;
 }
 
+/**
+ * 사용자의 계정 정보를 나타내는 인터페이스
+ *
+ * - `email`: 사용자의 이메일 주소
+ * - `password`: 사용자의 비밀번호
+ */
 export interface Account {
   email: string;
   password: string;
 }
 
+/**
+ * 사용자의 인증 정보를 나타내는 인터페이스
+ *
+ * - `accessToken`: 액세스 토큰
+ * - `refreshToken`: 리프레시 토큰
+ */
 export interface Authentication {
-  accessToken: string;
-  refreshToken: string;
+  accessToken: string; // 액세스 토큰
+  refreshToken: string; // 리프레시 토큰
 }
 
+/**
+ * 사용자의 프로필 정보를 나타내는 인터페이스
+ *
+ * - `subPhotos`: 부가 사진 경로 배열
+ * - `interests`: 관심사 배열
+ * - `rating`: 평점
+ * - `sexualPreference`: 성적 취향
+ * - `introduction`: 자기 소개
+ */
 export interface Profile {
-  subPhotos: string[];
-  interests: Interests[];
-  rating: number;
-  sexualPreference: string;
-  introduction: string;
+  subPhotos: string[]; // 부가 사진 경로 배열
+  interests: Interests[]; // 관심사 배열
+  rating: number; // 평점
+  sexualPreference: string; // 성적 취향
+  introduction: string; // 자기 소개
 }
 
+/**
+ * 사용자의 사진 정보를 나타내는 인터페이스
+ *
+ * - `mainPhoto`: 주요 사진 경로
+ */
 export interface Photo {
-  mainPhoto: string;
+  mainPhoto: string; // 주요 사진 경로
 }
 
+/**
+ * 추가적인 사용자 정보를 나타내는 인터페이스
+ *
+ * - `fancy`: 추가 정보 1
+ * - `distance`: 추가 정보 2
+ */
 export interface Another {
-  fancy: boolean;
-  distance: number;
+  fancy: boolean; // 추가 정보 1
+  distance: number; // 추가 정보 2
 }
 
+/**
+ * 사용자의 위치 정보를 나타내는 인터페이스
+ *
+ * - `latitude`: 위도
+ * - `longitude`: 경도
+ */
 export interface Position {
-  latitude: number;
-  longitude: number;
+  latitude: number; // 위도
+  longitude: number; // 경도
 }
--- app/redux/interface/submodules/utilInterface.ts
+++ app/redux/interface/submodules/utilInterface.ts
@@ -1,15 +1,32 @@
+/**
+ * 채팅 메시지를 나타내는 인터페이스
+ *
+ * - `id`: 메시지의 고유 식별자
+ * - `name`: 보낸 사용자의 이름
+ * - `time`: 메시지를 보낸 시간
+ */
 export interface Message {
-  id: string;
-  name: string;
-  time: string;
+  id: string; // 메시지의 고유 식별자
+  name: string; // 보낸 사용자의 이름
+  time: string; // 메시지를 보낸 시간
 }
 
+/**
+ * 채팅 상태 정보를 나타내는 인터페이스
+ *
+ * - `messages`: 채팅 메시지 배열
+ * - `connect`: 연결 여부
+ * - `noti`: 알림 여부
+ */
 export interface Chatting {
-  messages: Message[];
-  connect: boolean;
-  noti: boolean;
+  messages: Message[]; // 채팅 메시지 배열
+  connect: boolean; // 연결 여부
+  noti: boolean; // 알림 여부
 }
 
+/**
+ * 사용자 관심사를 정의하는 열거형
+ */
 export enum Interests {
   SPORTS = 1 << 0,
   TRAVEL = 1 << 1,
@@ -26,15 +43,30 @@ export enum Interests {
   DRINK = 1 << 12
 }
 
+/**
+ * 검색 조건을 나타내는 인터페이스
+ *
+ * - `ageRange`: 나이 범위 배열
+ * - `distance`: 거리
+ * - `fame`: 명성
+ * - `interests`: 관심사 배열
+ */
 export interface SearchParams {
-  ageRange: number[];
-  distance: number;
-  fame: number;
-  interests: Interests[];
+  ageRange: number[]; // 나이 범위 배열
+  distance: number; // 거리
+  fame: number; // 명성
+  interests: Interests[]; // 관심사 배열
 }
 
+/**
+ * 회원가입 단계를 나타내는 인터페이스
+ *
+ * - `emailCheck`: 이메일 확인 단계 여부
+ * - `profileCheck`: 프로필 확인 단계 여부
+ * - `emojiCheck`: 이모티콘 확인 단계 여부
+ */
 export interface RegisterSteps {
-  emailCheck: boolean;
-  profileCheck: boolean;
-  emojiCheck: boolean;
+  emailCheck: boolean; // 이메일 확인 단계 여부
+  profileCheck: boolean; // 프로필 확인 단계 여부
+  emojiCheck: boolean; // 이모티콘 확인 단계 여부
 }
