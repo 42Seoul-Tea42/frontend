16ca09fe3c98fb3e68370ce08a3360ad6a4e4bea
JaeJunday <wowns8114@gmail.com>
2024-04-13 20:49:32 +0900

    chore reactStyle warning remove

--- app/svg/HeartSVG.tsx
+++ app/svg/HeartSVG.tsx
@@ -15,9 +15,9 @@ const HeartSVG: React.FC<HeartSVGProps> = ({ color }) => {
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
       />
     </svg>
