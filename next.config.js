8ad0c9e982f17fe4b2f03c2c5507c0649ba68882
JaeJunday <wowns8114@gmail.com>
2024-05-01 13:35:00 +0900

    chore 개발 테스트용 프록시 구문추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  output: 'export',
+  // nginx 빌드용
+  // output: 'export',
   images: {
     unoptimized: true
   },
+  // next.config.js
+
+  // 개발 서버용 백엔드로 프록시하는 방법을 설정합니다.
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://localhost:5000/:path*' // 모든 요청을 로컬 호스트의 백엔드로 프록시합니다.
+      }
+    ];
+  }
 };
