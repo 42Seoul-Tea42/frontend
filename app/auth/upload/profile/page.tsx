75fd1b4810584d33a2dba79750bd31c1375a056c
JaeJunday <wowns8114@gmail.com>
2024-05-02 13:43:00 +0900

    style 컨텐츠와 버튼 사이 마진 조정

--- app/auth/upload/profile/page.tsx
+++ app/auth/upload/profile/page.tsx
@@ -75,6 +75,7 @@ const Profile = () => {
           관심있는 분야를 선택해주세요.
         </h5>
         <TagSelector />
+        <div className="mb-5"></div>
         <SubmitButton text="Submit" />
       </form>
     </div>
