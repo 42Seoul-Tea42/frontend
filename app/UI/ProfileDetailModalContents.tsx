a3e27315aed3ac2a56795dc836350469d449cd41
JaeJunday <wowns8114@gmail.com>
2024-05-09 17:19:04 +0900

    style 프로필 디테일 모달 화면 축소/확대 시 요소 크기 수정

--- app/UI/ProfileDetailModalContents.tsx
+++ app/UI/ProfileDetailModalContents.tsx
@@ -22,21 +22,25 @@ const ProfileDetailModalContents: React.FC<ProfileDetailModalContentsProps> = ({
   return (
     <div className="md:grid md:grid-cols-2 items-center gap-8">
       <div>{carousel}</div>
-      <div>
+      <div className="">
         <div className="ml-5 grid grid-cols-2">
           {items &&
             items.map((item, index) => (
-              <div key={index} className="mb-2">
-                <h6 className="shadow-sm sm:rounded-lg bg-gray-200 w-36 font-semibold text-gray-700">{item.title}</h6>
-                <div className="shadow-sm sm:rounded-lg w-36">{item.content}</div>
+              <div key={index} className="mb-2 p-2">
+                <div className="flex flex-col items-center mr-3">
+                  <h6 className="shadow-sm sm:rounded-lg bg-gray-200 w-36 font-semibold text-gray-700">{item.title}</h6>
+                  <div className="shadow-sm sm:rounded-lg w-36">{item.content}</div>
+                </div>
               </div>
             ))}
         </div>
-        <textarea
-          readOnly
-          className="ml-5 rounded-xl min-h-[100px] w-[310px] border-1 border-gray-400 text-gray-700"
-          value={introduciotn.title + '\n' + introduciotn.content}
-        ></textarea>
+        <div className="flex justify-center">
+          <textarea
+            readOnly
+            className="rounded-xl min-h-[100px] w-[310px] border-1 border-gray-400 text-gray-700"
+            value={introduciotn.title + '\n' + introduciotn.content}
+          ></textarea>
+        </div>
         <div className="flex justify-end mr-5 mt-10">
           <div className="flex items-center text-blue-400">
             {block}
