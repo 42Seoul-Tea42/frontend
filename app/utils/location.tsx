a25215703268ba6c8c2cb8d715c15f492d550d83
JaeJunday <wowns8114@gmail.com>
2024-05-09 16:57:07 +0900

    refactor 유저 위치 수집 로직 location으로 이동

--- app/utils/location.tsx
+++ app/utils/location.tsx
@@ -3,7 +3,7 @@ interface Coordinates {
   longitude: number;
 }
 
-function getGeoLocation(): Promise<Coordinates> {
+async function getGeoLocation(): Promise<Coordinates> {
   return new Promise((resolve, reject) => {
     if (!navigator.geolocation) {
       reject(new Error('Geolocation is not supported by this browser.'));
@@ -21,4 +21,13 @@ function getGeoLocation(): Promise<Coordinates> {
   });
 }
 
-export default getGeoLocation;
+async function requestUserLocation() {
+  try {
+    const { latitude, longitude } = await getGeoLocation();
+    localStorage.setItem('user-location', JSON.stringify({ latitude, longitude }));
+  } catch (error) {
+    alert('위치 권한 정보를 허용해주세요. 검색결과가 부정확해질 수 있습니다.');
+  }
+}
+
+export default requestUserLocation;
