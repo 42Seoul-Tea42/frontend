00a6dd9218e39c6a15ba03b55ec96ee1016c7fab
JaeJunday <wowns8114@gmail.com>
2024-05-24 13:59:59 +0900

    style 버튼 적용시 스타일 변경

--- app/auth/signup/components/InterestsButton.tsx
+++ app/auth/signup/components/InterestsButton.tsx
@@ -13,11 +13,12 @@ const InterestsButton: React.FC<InterestsButtonProps> = ({ text, onClick, value 
   return (
     <button
       type="button"
-      className={`${
-        interests?.includes(value) && 'border-2 border-blue-500'
-      } relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium bg-gray-300 text-gray-900 rounded-lg group 
-                   hover:text-white dark:text-white focus:ring-4 focus:outline-none`}
+      className="relative inline-flex border border-gray-700 m-1 items-center justify-center rounded-lg group"
       onClick={onClick}
+      style={{
+        border: interests?.includes(value) ? '2px solid transparent' : '',
+        borderImage: interests?.includes(value) ? 'linear-gradient(45deg, #FFD700, #FF8C00) 1' : ''
+      }}
     >
       <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
         {text}
