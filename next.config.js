ee00cded163dbb7c1cbfc34998edbacaff6e961f
JaeJunday <wowns8114@gmail.com>
2024-06-04 20:40:05 +0900

    chore:

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   // 개발 서버용 백엔드로 프록시하는 방법을 설정합니다.
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/favicon.ico',
-  //       destination: 'http://10.14.7.1:80/'
-  //     }
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: 'http://10.14.7.2:80/'
+      }
+    ];
+  },
 
   // nginx 빌드용
   // output: 'export',
