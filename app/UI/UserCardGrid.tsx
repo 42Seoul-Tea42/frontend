2dd96549a2e2123d8f872d03cb61b3d048f2a3c0
JaeJunday <wowns8114@gmail.com>
2024-06-19 23:00:47 +0900

    style 화면 사이즈에 따라서 한 행에서 보이는 카드 개수변경

--- app/UI/UserCardGrid.tsx
+++ app/UI/UserCardGrid.tsx
@@ -2,7 +2,7 @@ type UserCardGridProps = {
   items: JSX.Element;
 };
 const UserCardGrid: React.FC<UserCardGridProps> = ({ items }) => {
-  return <div className="grid grid-cols-2 md:grid-cols-3 gap-10">{items}</div>;
+  return <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-10">{items}</div>;
 };
 
 export default UserCardGrid;
