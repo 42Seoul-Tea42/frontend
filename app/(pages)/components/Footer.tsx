25a9ce7a8b8d7a50f33c5a3db0046986e4100080
JaeJunday <wowns8114@gmail.com>
2024-06-21 18:33:00 +0900

    fix 푸터 요소 한 화면에 고정되도록 수정
    - 푸터의 컨텐츠의 재조정 필요. 시간을 많이 사용하지말고 꼭 필요하다고 느껴지는것만 넣을것

--- app/(pages)/components/Footer.tsx
+++ app/(pages)/components/Footer.tsx
@@ -14,28 +14,8 @@ const Footer = () => {
   };
 
   return (
-    <footer>
-      <ul className="flex flex-col border-t sm:flex-row items-center justify-center px-5 py-5 text-md font-medium text-gray-500sm:mt-0">
-        <li className="mx-5 mb-5 sm:mb-0">
-          <button onClick={() => {}} className="hover:underline">
-            후원하기
-          </button>
-        </li>
-        <li className="mx-5 mb-5 sm:mb-0">
-          <button onClick={() => {}} className="hover:underline">
-            약관
-          </button>
-        </li>
-        <li className="mx-5 mb-5 sm:mb-0">
-          <button onClick={() => {}} className="hover:underline">
-            개인정보 이용방침
-          </button>
-        </li>
-        <li className="mx-5 mb-5 sm:mb-0">
-          <button onClick={() => {}} className="hover:underline">
-            고객센터
-          </button>
-        </li>
+    <footer className="fixed bottom-0 w-full border-t bg-white">
+      <ul className="flex sm:flex-row grid-cols-2 flex-col items-center justify-center px-5 py-5 text-md font-medium text-gray-500 sm:mt-0">
         <li className="mx-5 sm:mb-0">
           <button onClick={handleLogout} className="hover:underline">
             로그아웃
