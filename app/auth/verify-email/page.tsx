fef5099feef68f40c179bf3a8eb0d62aa61383d2
JaeJunday <wowns8114@gmail.com>
2024-06-21 18:52:45 +0900

    feat 이메일 인증페이지 최종구현

--- app/auth/verify-email/page.tsx
+++ app/auth/verify-email/page.tsx
@@ -1,44 +1,64 @@
 'use client';
-import Indicator from '@/(pages)/components/Indicator';
-import { EmailInput, LoginIdInput, PasswordInput } from '@/(pages)/forms';
+import { EmailInput } from '@/(pages)/forms';
 import CardForm from '@/(pages)/forms/CardForm';
 import { SubmitButton } from '@/ui';
 import { getResendEmail } from '@/redux/slices/loginSlice';
-import { getCheckDuplicateEmail, setIsEmailDuplicateChecked } from '@/redux/slices/signupSlice';
 import { RootState } from '@/redux/store';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { getMyEmail } from '@/redux/slices/accountSlice';
+import { changeMyEmail, getMyEmail } from '@/redux/slices/accountSlice';
+import Indicator from '@/(pages)/components/Indicator';
+import { getCheckDuplicateEmail } from '@/redux/slices/signupSlice';
+import { RefreshSVG } from '@/svg/RefreshSVG';
 
 function emailVerify() {
   const dispatch = useDispatch();
-
-  const email = useSelector((state: RootState) => state.accountSlice.user.email);
-  const changeEmail = () => {};
+  const validation = useSelector((state: RootState) => state.signupSlice.validation);
+  const viewMail = useSelector((state: RootState) => state.accountSlice.viewMail);
+  const changeEmail = () => {
+    dispatch<any>(changeMyEmail(null));
+  };
 
   useEffect(() => {
     dispatch<any>(getMyEmail());
   }, []);
+
   return (
     <CardForm
-      onSubmit={() => {}}
       subject={'이메일 인증이 필요합니다.'}
       inputs={
         <div className="mb-10 space-y-5">
-          <p className="text-lg font-bold">현재이메일 : {email}</p>
-          <h3 className="text-lg font-bold">이메일 바꾸기</h3>
-          <div className="border">
-            <LoginIdInput />
-            <PasswordInput />
-            <EmailInput />
-            <SubmitButton text="이메일 변경" onClick={changeEmail} />
+          <div className="flex space-x-2">
+            <p className="text-lg font-md">현재이메일 : {viewMail}</p>
+            <button className="" onClick={() => dispatch<any>(getMyEmail())}>
+              <RefreshSVG />
+            </button>
           </div>
-
-          <h3 className="text-bold text-lg">혹시 이메일을 못받으셨나요? </h3>
+          <details>
+            <summary className="text-blue-600 mb-4">이메일을 잘못입력하셨나요?</summary>
+            <h3 className="text-md font-md">이메일 바꾸기</h3>
+            <div className="border p-4 rounded-xl">
+              <EmailInput
+                addJSX={
+                  <Indicator
+                    // 이메일 중복체크
+                    onClick={() => dispatch<any>(getCheckDuplicateEmail())}
+                    color={validation.isEmailDuplicateChecked ? 'bg-green-500' : 'hover:opacity-50 bg-red-500'}
+                    text={validation.isEmailDuplicateChecked ? '' : 'click'}
+                  />
+                }
+              />
+              <SubmitButton text="이메일 변경" onClick={changeEmail} />
+            </div>
+          </details>
+          <details>
+            <summary className="text-blue-600 mb-4">이메일을 못받으셨나요?</summary>
+            <div className="border p-4 rounded-xl">
+              <SubmitButton text="이메일 다시보내기" onClick={() => dispatch<any>(getResendEmail())} />
+            </div>
+          </details>
         </div>
       }
-      //test
-      button={<SubmitButton text="이메일 다시보내기" onClick={() => dispatch<any>(getResendEmail())} />}
     />
   );
 }
