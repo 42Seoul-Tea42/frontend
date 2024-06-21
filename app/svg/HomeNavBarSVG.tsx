b2ac80ad83484d41327d46cb9a1fefb4059ed0d6
JaeJunday <wowns8114@gmail.com>
2024-06-21 18:39:49 +0900

    feat SVG 2종 추가
    - refresh
    - adjust

--- app/svg/HomeNavBarSVG.tsx
+++ app/svg/HomeNavBarSVG.tsx
@@ -93,7 +93,7 @@ export const HistorySVG: React.FC = () => {
 export const UserSVG: React.FC = () => {
   return (
     <svg
-      className="w-6 h-6"
+      className="w-7 h-7"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
@@ -123,5 +123,26 @@ export const HamburgerSVG: React.FC = () => {
         />
       </svg>
     </div>
+  );
+};
+
+export const AdjustSVG: React.FC = () => {
+  return (
+    <svg
+      className="w-6 h-6 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeWidth="2"
+        d="M20 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6h-2m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4"
+      />
+    </svg>
   );
 };
