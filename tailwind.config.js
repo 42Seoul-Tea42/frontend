2f3f61c52a2934718909064e37a8efe5ee9d292d
JaeJunday <wowns8114@gmail.com>
2024-06-26 20:51:08 +0900

    fix tailwind legacy syntax error remove

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   mode: 'jit',
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './pages/**/*.{ts,tsx}', './public/**/*.html'],
-  purge: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './pages/**/*.{ts,tsx}', './public/**/*.html'],
   plugins: [require('flowbite/plugin')],
   theme: {
     extend: {}
