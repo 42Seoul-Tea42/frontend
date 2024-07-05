e4b382f62b84c08e8bae69ab9f8e3950667242a1
JaeJunday <wowns8114@gmail.com>
2024-07-05 18:08:56 +0900

    chore 자리를 바꾸면 private ip가 바뀌는 문제를 해결하기 위해 .env로 변경

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ module.exports = {
     return [
       {
         source: '/favicon.ico',
-        destination: 'http://10.12.5.2:80/'
+        destination: process.env.NEXT_PUBLIC_DOMAIN
       }
     ];
   },
 
   images: {
     unoptimized: true
-  },
+  }
 
   // // 리렌더링 방지 true 하면 두번씩 렌더링됨
   // reactStrictMode: false,
@@ -23,10 +23,4 @@ module.exports = {
 
   // nginx 빌드용
   // output: 'export',
-
-  env: {
-    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
-    NEXT_PUBLIC_KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
-    NEXT_PUBLIC_KAKAO_REDIRECT_URI: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI
-  }
 };
